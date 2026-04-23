9992412bba116cfa915544b364c8657b3e16c9b3
Ajayi Oluwaseyi <252756675+Oluwaseyiajayi-dev@users.noreply.github.com>
2026-04-23 04:58:34 -0700

    Add form submission alert and fix event listener

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector("button").addEventListener("click", function() {
     alert("Thanks for reaching out! i will get back to you soon.")
 });
+document.querySelector("form").addEventlistener("submit", function(e) {
+    e.preventDefault();
+    alert("Message sent successfully");
+});
